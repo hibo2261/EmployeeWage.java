ab178d9d7721b30f1858e0e7a8b997e43375349f
hibo2261 <hborse1@gmail.com>
2022-08-29 02:38:43 +0530

    [Himanshu]:Added Employee Wage Switch Statement

--- src/com/EmployeeWage/EmployeeWage.java
+++ src/com/EmployeeWage/EmployeeWage.java
@@ -1,29 +1,28 @@
 package com.EmployeeWage;
 
 public class EmployeeWage {
-    public static void main(String args [])
-    {
-
-        System.out.println("Welcome to Emplyoee Wage Computation Program");
-
-
-        int isPresent = 1;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage");
+        // TODO Auto-generated method stub
+        int employeePresent = 1;
+        int employeePartTime = 2;
         int wagePerHour = 20;
-        int noOfWorkingHours = 8;
-        int partTimeHours = 4;
-        int checkAttendance =(int)Math.floor((Math.random()*10)%3);
-        System.out.println(checkAttendance);
-        if(checkAttendance == 1) {
-            System.out.println("Emplyoee is present");
-            System.out.println("Emplyoee Daily wage is" + wagePerHour * noOfWorkingHours);
+        int numberOfHours = 8;
+        int numberOfHoursForPartTime = 4;
+        int isPresent = (int) Math.floor(Math.random() * 3);
+        switch (isPresent) {
+            case 0:
+                System.out.println("Employee is Present");
+                System.out.println("Employee earn = " + numberOfHours * wagePerHour);
+                break;
+            case 1:
+                System.out.println("Employee is doing Part Time");
+                System.out.println("Employee earn = " + numberOfHoursForPartTime * wagePerHour);
+                break;
+            case 2:
+                System.out.println("Employee is not Present");
+                System.out.println("Employee earn = 0");
+                break;
         }
-        else if(checkAttendance == 0) {
-            System.out.println("Emplyoee is absent");
-        }
-        else {
-            System.out.println("Part Time");
-            System.out.println("Emplyoee Part Time wage is" + wagePerHour * partTimeHours);
-        }
-
     }
 }
